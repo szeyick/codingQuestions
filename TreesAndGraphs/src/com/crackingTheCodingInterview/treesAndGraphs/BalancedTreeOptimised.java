141f0490573e7979d53da3409e05de261b9c4ef8
Sze Yick <szeyick@gmail.com>
2015-02-02 22:43:47 +1100

    Update of TreesAndGraphs
    Changes -
    
    1. treeBalancing
    - Updated notes on optimised tree balancing algorithm. The key is to
    check the height and to see if tree is balanced in the same method. If
    tree is unbalanced return its height as -1.
    
    2. DirectedGraph
    - Wrote question to implement a search of a graph to see if a node
    exists. The solution would be to implement either a BFS or DFS.

--- TreesAndGraphs/src/com/crackingTheCodingInterview/treesAndGraphs/BalancedTreeOptimised.java
+++ TreesAndGraphs/src/com/crackingTheCodingInterview/treesAndGraphs/BalancedTreeOptimised.java
@@ -5,10 +5,14 @@ import com.crackingTheCodingInterview.treesAndGraphs.tree.Node;
 /**
  * The {@link BalancedTreeOptimised}
  * <p>
- * This is an extension on the {@link BalancedTree}
- * where we optimise the isBalanced() method to
- * record the height of each subtree as we go along
- * rather than calling a height() method separately.
+ * This class represents an optimised version of the
+ * tree balancing algorithm. The idea in this version 
+ * is to push both the height finding and checking onto
+ * the same method checkHeight(...) rather than performing
+ * them in two different methods. This reduces the overall 
+ * time complexity of the algorithm to O(N) since we do not
+ * need to additionally iterate through the tree to check
+ * the height and balance each time
  * <p>
  * @author szeyick
  */
@@ -21,31 +25,73 @@ public class BalancedTreeOptimised {
 	public static void main(String[] args) {
 		Node tree = buildTree();
 		
-		isBalanced(tree, new HeightCount());
+		System.out.println("Is tree balanced - " + isBalanced(tree));
+	}
+	
+	/**
+	 * Check the height of the tree.
+	 * <p>
+	 * This method works by recursing through the tree and checking
+	 * both the height and balance at the same time. The height is checked
+	 * by calling checkHeight on its left and right children, where the balance
+	 * is called by determining whether the difference in height of its left 
+	 * and right children is greater than 1.
+	 * @param currentNode - The current root of the tree/subtree.
+	 * @return -1 if the tree is unbalanced, otherwise return its actual height.
+	 */
+	private static int checkHeight(Node currentNode) {
+		
+		// Base case (if tree is empty)
+		if (currentNode == null) {
+			return 0;
+		}
+		
+		// Find the height of its left and right children and check if the subtrees are balanced.
+		int leftHeight = checkHeight(currentNode.leftChild);
+		if (leftHeight == -1) {
+			return -1; // Immediately return -1 if the height is -1;
+		}
+		int rightHeight = checkHeight(currentNode.rightChild);
+		if (rightHeight == -1) {
+			return -1;
+		}
+		
+		int heightDifference = 0; // Check to see if the current node is balanced.
+		// This check is done to ensure that we're subtracting the smaller from the larger so
+		// we don't have a negative height.
+		if (leftHeight > rightHeight) {
+			heightDifference = leftHeight - rightHeight;
+		}
+		else {
+			heightDifference = rightHeight - leftHeight;
+		}
+		// Return the height if the difference is at most 1.
+		if (heightDifference <= 1) {
+			return Math.max(leftHeight, rightHeight) + 1;
+		}
+		else {
+			return -1; // Returning -1 means the tree is unbalanced.
+		}
 	}
 	
 	/**
 	 * Evaluate to determine if a tree is balanced.
 	 * <p>
-	 * The idea here is to maintain references to the current height of
-	 * the tree as we recurse along. 
+	 * The idea here would be to get the checkHeight() method to 
+	 * do all the recursing through the tree. 
 	 * <p>
 	 * @param currentNode - The current node visited.
-	 * @param height - The current aggregate height of the tree.
 	 * @return <code>true</code> if the tree is balanced, false otherwise.
+	 * The tree is unbalanced if the returned value from checkHeight is -1,
+	 * otherwise any other value will be the actual height of the tree.
 	 */
-	private static boolean isBalanced(Node currentNode, HeightCount height) {
-		
-		if (currentNode == null) {
+	private static boolean isBalanced(Node currentNode) {
+		if(checkHeight(currentNode) == -1) {
 			return false;
 		}
-		
-		// How to know
-		// Increment the count for the left
-		isBalanced(currentNode.leftChild, height);
-		isBalanced(currentNode.rightChild, height);
-		
-		return false;
+		else {
+			return true;
+		}
 	}
 
 	/**
@@ -56,7 +102,7 @@ public class BalancedTreeOptimised {
 		Node root = createNode(5);
 		root.leftChild = createNode(2);
 		root.rightChild = createNode(6);
-		root.rightChild.rightChild = createNode(7);
+//		root.rightChild.rightChild = createNode(7);
 		root.leftChild.leftChild = createNode(1);
 		root.leftChild.rightChild = createNode(4);
 		root.leftChild.rightChild.leftChild = createNode(3);
@@ -71,17 +117,5 @@ public class BalancedTreeOptimised {
 	 */
 	private static Node createNode(int value) {
 		return new Node(value);
-	}
-	
-	/**
-	 * 
-	 * @author szeyick
-	 *
-	 */
-	private static class HeightCount {
-		
-		int leftHeight;
-		
-		int rightHeight;
 	}
 }
---
+++ TreesAndGraphs/src/com/crackingTheCodingInterview/treesAndGraphs/DirectedGraph.java
@@ -0,0 +1,28 @@
+package com.crackingTheCodingInterview.treesAndGraphs;
+
+/**
+ * The {@link DirectedGraph}
+ * <p>
+ * Given a directed graph, design an algorithm to find out
+ * whether there is a route between two nodes.
+ * <p>
+ * This question involves a start node and a finish node, and
+ * the operation to implement is a "search". If you can find
+ * the second node from the first one, it means that there is a
+ * path between the two. It may also be beneficial to ask as to
+ * whether the nodes are close together or not as it may be more
+ * efficient to implement a DFS or BFS depending on how closely the
+ * nodes are together or how big the graph is
+ * <p>
+ * @author szeyick
+ */
+public class DirectedGraph {
+	
+	/**
+	 * The program main.
+	 * @param args - Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
