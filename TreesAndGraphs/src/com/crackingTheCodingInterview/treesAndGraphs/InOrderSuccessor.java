978d3a657182739b90b1208e9c0a7fd949c4ba73
Sze Yick <szeyick@gmail.com>
2015-02-05 22:18:34 +1100

    TreesAndGraphs
    Changes -
    
    1. InOrderSuccessor
    - Initial delivery of project code to find the in order successor of a
    node.

---
+++ TreesAndGraphs/src/com/crackingTheCodingInterview/treesAndGraphs/InOrderSuccessor.java
@@ -0,0 +1,28 @@
+package com.crackingTheCodingInterview.treesAndGraphs;
+
+/**
+ * The {@link InOrderSuccessor}
+ * <p>
+ * Write an algorithm to find the "next" node or its
+ * in-order successor of a given node in a binary search
+ * tree. You may assume that each node has a link to its
+ * parent.
+ * <p>
+ * The link to its parent will allow us to determine if the
+ * current node is a left or right child of its parent. If it
+ * is the left child, its "next" is its parent, whereas if it
+ * is the right child, its "next" will be the smallest child
+ * of its grandparent. 
+ * <p>
+ * @author szeyick
+ */
+public class InOrderSuccessor {
+
+	/**
+	 * The program main.
+	 * @param args - Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
