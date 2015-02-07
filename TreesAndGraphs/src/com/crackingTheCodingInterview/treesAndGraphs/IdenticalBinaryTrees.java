7de5b565541a47467f330550f7e7939f0b78d0ca
Sze Yick <szeyick@gmail.com>
2015-02-07 23:44:16 +1100

    TreesAndGraphs
    Changes -
    
    1. IdenticalBinaryTrees
    - Initial delivery of code.

---
+++ TreesAndGraphs/src/com/crackingTheCodingInterview/treesAndGraphs/IdenticalBinaryTrees.java
@@ -0,0 +1,25 @@
+package com.crackingTheCodingInterview.treesAndGraphs;
+
+/**
+ * The {@link IdenticalBinaryTrees}
+ * <p>
+ * You have two very large binary trees: T1, with millions of 
+ * nodes and T2 with hundreds of nodes. Create an algorithm to
+ * decide if T2 is a subtree of T1.
+ * 
+ * A tree T2 is a subtree of T1 if there exists a node n in T1 such
+ * that the subtree of n is identical to T2. That is, if you cut off 
+ * the tree at node n, the two trees would be identical.
+ * <p>
+ * @author szeyick
+ */
+public class IdenticalBinaryTrees {
+
+	/**
+	 * The program main.
+	 * @param args - Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
