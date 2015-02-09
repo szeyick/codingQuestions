826bcfd5ffa6dd1ede9c20373d9c5ffae6f4c224
Sze Yick <szeyick@gmail.com>
2015-02-09 23:02:24 +1100

    TreesAndGraphs
    Changes -
    
    1. sumValues
    - Added initial code structure and notes.

---
+++ TreesAndGraphs/src/com/crackingTheCodingInterview/treesAndGraphs/SumValues.java
@@ -0,0 +1,21 @@
+package com.crackingTheCodingInterview.treesAndGraphs;
+
+/**
+ * The {@link SumValues}
+ * <p>
+ * You ar egiven a binary tree in which each node contains a value. Design 
+ * an algorithm to print all paths which sum to a given value. The path does
+ * not need to start or end at the root or leaf.
+ * <p>
+ * @author szeyick
+ */
+public class SumValues {
+
+	/**
+	 * The program main.
+	 * @param args - Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
