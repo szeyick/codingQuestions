9cf56b93ed18c26721f97830134bb059e8221403
Sze Yick <szeyick@gmail.com>
2015-02-21 20:39:59 +1100

    Updated BitManipulation
    Changes -
    
    1. binaryRepresentationLessThanOne
    - Updated notes on how to represent values less than 1 in binary.
    - A hint here is that Math.pow(a,b) needs to return a double as if it
    returns an integer it will return 0.
    2. binaryRpresentationLessThanOne.java
    - Updated code to solve the algorithm to represent values less than 1
    in binary.

--- BitManipulation/src/com/crackingTheCodingInterview/bitManipulation/BinaryRepresentationLessThanOne.java
+++ BitManipulation/src/com/crackingTheCodingInterview/bitManipulation/BinaryRepresentationLessThanOne.java
@@ -26,7 +26,63 @@ public class BinaryRepresentationLessThanOne {
 	 * @param args - Command line arguments.
 	 */
 	public static void main(String[] args) {
-		double value = 1 * (1 / Math.pow(2, 2));
-		System.out.println(value);
+		// Binary representation of 0.75
+		System.out.println("Binary representation of 0.75 is  - " + decimalToBinary(0.75));
+		
+		// Binary representation of 1 is 1.
+		System.out.println("Binary representation of 1 is  - " + decimalToBinary(1));
+		
+		// Cannot represent 0.1 within 32 bits.
+		System.out.println("Binary representation of 1 is  - " + decimalToBinary(0.1));
+	}
+	
+	/**
+	 * Convert a decimal value between 0 and 1 to binary.
+	 * @param value - The value to convert
+	 * @return - The binary value as a string.
+	 */
+	private static String decimalToBinary(double value) {
+		// Check if value is between 0 and 1
+		if (value <= 1 && value >= 0) {
+			// If values are 0 or 1 they don't need to be converted.
+			if (value == 0) {
+				return "0";
+			}
+			if (value == 1) {
+				return "1";
+			}
+			// Declare variables for the calculation
+			double remainder = value;
+			boolean is32BitValue = true;
+			int bitCount = 0;
+			int exponent = -1;
+			StringBuffer stringBuffer = new StringBuffer();
+			stringBuffer.append(".");
+			
+			while (remainder > 0) {
+				// Terminate if we've exceeded 32 bit representation
+				if (bitCount >= 32) {
+					is32BitValue = false;
+					break;
+				}
+				double bitValue = (double) Math.pow(2, exponent);
+				// Add 1 to the bit if it fits within our current value.
+				if (remainder - bitValue >= 0) {
+					stringBuffer.append("1");
+					remainder = remainder - bitValue; // Update the remainder
+				}
+				else {
+					stringBuffer.append("0"); // Add 0 if it doesn't fit within the current value.
+				}
+				// Proceed to next bit.
+				bitCount++;
+				exponent--;
+			}
+			if (is32BitValue) {
+				return stringBuffer.toString();
+			}
+			return "Error - Cannot be represented within 32 bits.";
+		}
+		return "Error - Value is not between 0 and 1";
 	}
 }
